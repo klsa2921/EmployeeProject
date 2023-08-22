52903d6b37b2753d8e3e129d279ec7fdfa997a3f
klsa2921 <klsa.mallikanti2921@gmail.com>
2023-08-22 23:01:54 +0400

    Revert "authentication working"
    This reverts commit bd78c48f270f41aa1986645d0d9d5c3e4df40b79.

--- EmployeeSpring/src/main/java/com/example/employeespring/authentication/AuthenticationController.java
+++ EmployeeSpring/src/main/java/com/example/employeespring/authentication/AuthenticationController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthenticationController {
 
-    @Autowired
     private  AuthenticationService authenticationService;
 
     @PostMapping("/register")
