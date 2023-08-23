e9e84b6497d33265bba51808c4c039297a0a28b1
klsa2921 <klsa.mallikanti2921@gmail.com>
2023-08-23 07:52:52 +0400

    22-08

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,23 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import {FormsModule} from "@angular/forms";
-import {RouterOutlet} from "@angular/router";
-import { EmployeeComponent } from './employee/employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    EmployeeComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    RouterOutlet
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
